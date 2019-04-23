ab1adf62e807ab2ea923d872abdc6f6239c5a27e
Dani Santos <santos.adaniele@gmail.com>
2019-04-23 12:29:10 +0200

    add initial files to server

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' });
+});
+
+const PORT = process.env.PORT || 5000
+// only works in production
+// the or will say the default is 5000
+// 5000 will be in development
+
+app.listen(PORT);
