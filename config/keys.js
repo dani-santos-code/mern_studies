b9aa38755aa90e0bb75a16d054bcf6465dfcf2fd
Dani Santos <santos.adaniele@gmail.com>
2019-04-23 14:19:19 +0200

    basi setting for google auth

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  googleClientID:
+    '1017362228477-kvcejfmupghkq8lf086oju6cdnmmfgsm.apps.googleusercontent.com',
+  googleClientSecret: 'AIzaSyD9bvWm0VDNhCQCseD1H2g9RDyZl57Pm-U'
+};
--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ bye: 'buddy' });
-});
+passport.use(new GoogleStrategy({
+    // clientID: process.env['GOOGLE_CLIENT_ID'],
+    // clientSecret: process.env['GOOGLE_CLIENT_SECRET'],
+    // scope: 'profile'
+}));
+//new instance of Google Strategy
+// passport.use - I want you to be aware that there's a new strategy.
 
 const PORT = process.env.PORT || 5000
-// only works in production
-// the or will say the default is 5000
-// 5000 will be in development
+/* only works in production
+ the or will say the default is 5000
+ 5000 will be in development */
 
 app.listen(PORT);
